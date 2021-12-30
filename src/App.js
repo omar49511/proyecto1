ce1b35bcc69f88ebb7e3fb4df11f690317a591e4
omar <omarreyes49511@gmail.com>
2021-12-30 10:31:34 -0800

    se crearon los textbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// componente de react
+
 
 function App() {
+  const [linea1,setLinea1] = useState('');
+  const [linea2,setLinea2] = useState('');
+  const onChangeLinea1 = function(evento) {
+    setLinea1(evento.target.value)
+  }
+  const onChangeLinea2 = function(evento) {
+    setLinea2(evento.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select>
+        <option value = "fire">Casa en llamas</option>
+      </select><br/>
+      <input onChange={onChangeLinea1} type="text" placeholder='linea 1'/><br/>
+      <input onChange={onChangeLinea2} type="text" placeholder='linea 2'/><br/>
+      <button>Exportar</button>
+      <div>
+        <span>{linea1}</span><br/>
+        <span>{linea2}</span><br/>
+      </div>
     </div>
   );
 }
